b64bd6ba678f9377982c05730a2d92f0e1d73816
Anna Archive <ytpt@mail.ru>
2023-11-30 17:12:06 -0500

    Marked the day in calendar; Refactored code style;

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,36 +1,39 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from 'react';
+import DayState from './DayState';
+import { toggleDuty } from '../app/actions';
 
 function getDayInMonth(month: number, year: number) {
     const date = new Date(year, month, 1);
     const firstWeekDay = (date.getDay() + 6) % 7;
     const numberOfEmptyDays = Array(firstWeekDay).fill(null);
-    const days = [...numberOfEmptyDays]
+    const days = [...numberOfEmptyDays];
 
     while(date.getMonth() == month) {
-        days.push(new Date(date))
-        date.setDate(date.getDate() + 1)
+        days.push(new Date(date));
+        date.setDate(date.getDate() + 1);
     }
     return days;
 }
 
 const currentDate = new Date();
-const currentDay = currentDate.getDate();
 const currentMonth = currentDate.getMonth();
 const currentYear = currentDate.getFullYear();
 const weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
-export default  function Calendar() {
+export default function Calendar({ duty, dutyTime }: {
+    duty: string,
+    dutyTime: Record<string, boolean> | null
+}) {
     const [month, setMonth] = useState(currentMonth);
     const [year, setYear] = useState(currentYear);
     const [selectedDate, setSelectedDate] = useState(new Date());
     const [dayInMonth, setDayInMonth] = useState(getDayInMonth(currentMonth, currentYear));
 
-
     useEffect(() => {
         setDayInMonth(getDayInMonth(month, year));
-        setSelectedDate(new Date(year, month))
+        setSelectedDate(new Date(year, month));
     }, [month, year])
 
     const goToPrevMonth = () => {
@@ -52,9 +55,16 @@ export default  function Calendar() {
     }
 
     const getFullDate = () => {
-        return `${selectedDate.toLocaleString("en", {
-            month: "long",
-        })} ${selectedDate.getFullYear()}`;
+        return `${selectedDate.toLocaleString("en", {month: "long",})} ${selectedDate.getFullYear()}`;
+    }
+
+    const getCurrentDay = (day: Date) => {
+        return `${year.toString()}-${(month + 1)
+            .toString()
+            .padStart(2, "0")}-${day
+                .getDay()
+                .toString()
+                .padStart(2, "0")}`;
     }
 
     return (
@@ -73,10 +83,20 @@ export default  function Calendar() {
                     </div>
                 ))}
                 {dayInMonth.map((day, index) => (
-                    <div key={index} className="flex flex-col items-center p-2">
+                    <div
+                        key={index}
+                        className="flex flex-col items-center p-2 hover:cursor-pointer"
+                        onClick={() => toggleDuty({
+                            duty,
+                            dutyTime,
+                            date: getCurrentDay(day),
+                            done: dutyTime ? dutyTime[getCurrentDay(day)] : true,
+                        })}
+                    >
                         <span className="font-sans text-xs text-center font-light text-neutral-400">
                             {day?.getDate()}
                         </span>
+                        {day && <DayState day={dutyTime ? dutyTime[getCurrentDay(day)] : undefined}/>}
                     </div>
                 ))}
             </div>
--- components/DayState.tsx
+++ components/DayState.tsx
@@ -1,16 +1,16 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default  function DayState({ day }: {day: boolean | undefined}) {
-    let image: [string, string, number?] = ["/images/mark.svg", "gray mark", 12]
+    let image: [string, string, number?] = ["/images/mark.svg", "gray mark", 12];
 
-    if (day === true) image = ["/images/check.svg", "green mark", 24]
-    if (day === false) image = ["/images/x.svg", "red mark", 24]
+    if (day === true) image = ["/images/check.svg", "green mark", 24];
+    if (day === false) image = ["/images/x.svg", "red mark", 24];
 
-    const [src, alt, size] = image
+    const [src, alt, size] = image;
 
     return (
         <div className="flex items-center justify-center h-9">
-            <Image src={src} width={size} height={size} alt={alt} />
+            <Image src={src} width={size} height={size} alt={alt}/>
         </div>
     );
 };
